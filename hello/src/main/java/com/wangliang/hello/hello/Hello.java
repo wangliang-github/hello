9a04f716428ed3509345bf57a18093d7a6b6c808
wangliang <liang.wang@52ydwf.com>
2019-10-16 15:37:38 +0800

    hello

---
+++ hello/src/main/java/com/wangliang/hello/hello/Hello.java
@@ -0,0 +1,12 @@
+package com.wangliang.hello.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "HELLO!";
+    }
+}
