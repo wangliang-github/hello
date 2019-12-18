bd6b738bfa5be16caeaa3be566bb9e936886dc96
wangliang <liang.wang@52ydwf.com>
2019-12-18 16:20:52 +0800

    'demo上传用于jenkins'

--- hello/src/main/java/com/wangliang/hello/HelloApplication.java
+++ hello/src/main/java/com/wangliang/hello/HelloApplication.java
@@ -3,12 +3,20 @@ package com.wangliang.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloApplication {
+public class HelloApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~项目[app]已经启动");
 	}
 
 }
--- hello/src/main/java/com/wangliang/hello/hello/Hello.java
+++ hello/src/main/java/com/wangliang/hello/hello/Hello.java
@@ -1,11 +1,12 @@
 package com.wangliang.hello.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "HELLO!";
     }
